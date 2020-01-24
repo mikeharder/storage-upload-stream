752268c01f5ec8b93a0e0c7bec90fe6e9f7f6358
Mike Harder <mharder@microsoft.com>
2020-01-23 16:09:57 -0800

    Test server connection reset

---
+++ net/ForceReset/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace ForceReset
+{
+    class Program
+    {
+        private const int _port = 443;
+        private const int _bufferSize = 16384;
+
+        static void Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("STORAGE_CONNECTION_STRING");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Environment variable STORAGE_CONNECTION_STRING is not set");
+            }
+
+            var accountName = Regex.Match(connectionString, "AccountName=([^;]*)").Groups[1].Value;
+            var endpointSuffix = Regex.Match(connectionString, "EndpointSuffix=([^;]*)").Groups[1].Value;
+
+            var host = $"{accountName}.blob.{endpointSuffix}";
+
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            Log($"Connecting to {host}:{_port}...");
+            socket.Connect(host, _port);
+            Log($"Connected");
+
+            var buffer = new byte[_bufferSize];
+
+            Log($"Receiving up to {_bufferSize} bytes...");
+            var received = socket.Receive(buffer);
+            Log($"Received {received} bytes");
+        }
+        
+        private static void Log(string value)
+        {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] {value}");
+        }
+    }
+}
